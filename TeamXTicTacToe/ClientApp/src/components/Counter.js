a17670599865e6ceb713af259a19b02a54c7bf80
polga <polga@POLLY>
2020-11-11 18:03:52 -0800

    last updates Wednesday Night

--- TeamXTicTacToe/ClientApp/src/components/Counter.js
+++ TeamXTicTacToe/ClientApp/src/components/Counter.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button
+            className="square"
+            onClick={props.onClick}
+            size="lg"
+        >
             {props.value}
         </button>
     );
