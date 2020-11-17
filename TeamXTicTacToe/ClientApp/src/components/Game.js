a72943966550396770380dbd4e366e625b3f65ce
CMofjeld <cmofjeld@uw.edu>
2020-11-17 15:44:40 -0800

    Implemented first attempt at AI functions.

--- TeamXTicTacToe/ClientApp/src/components/Game.js
+++ TeamXTicTacToe/ClientApp/src/components/Game.js
@@ -180,3 +180,88 @@ function calculateWinner(squares) {
     }
     return null;
 }
+
+function minimax(reboard, player, winningPlayer) {
+    // iter++; Don't need this?
+    let array = avail(reboard);
+    var losingPlayer = winningPlayer === "X" ? "O" : "X";
+    if (winning(reboard, losingPlayer)) {
+        return {
+            score: -10
+        };
+    } else if (winning(reboard, winningPlayer)) {
+        return {
+            score: 10
+        };
+    } else if (array.length === 0) {
+        return {
+            score: 0
+        };
+    }
+
+    var moves = [];
+    for (var i = 0; i < array.length; i++) {
+        var move = {};
+        move.index = reboard[array[i]];
+        reboard[array[i]] = player;
+
+        if (player == winningPlayer) {
+            var g = minimax(reboard, losingPlayer, winningPlayer);
+            move.score = g.score;
+        } else {
+            var g = minimax(reboard, winningPlayer, winningPlayer);
+            move.score = g.score;
+        }
+        reboard[array[i]] = move.index;
+        moves.push(move);
+    }
+
+    var bestMove;
+    if (player === winningPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
+}
+
+//available spots
+function avail(reboard) {
+    var indiciesAvailable = [];
+    for (int i = 0; i < reboard.length; i++) {
+        if (reboard[i] == null) {
+            indiciesAvailable.push(i);
+        }
+    }
+    return indiciesAvailable;
+}
+
+// winning combinations
+function winning(board, player) {
+    if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
