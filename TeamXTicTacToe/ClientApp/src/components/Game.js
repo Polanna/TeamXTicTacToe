d2eca941adda0ca2ed6b485f3b35c2a2c9bd19bf
CMofjeld <cmofjeld@uw.edu>
2020-11-17 16:41:39 -0800

    Added comments for minimax and gave a more descriptive name to array.

--- TeamXTicTacToe/ClientApp/src/components/Game.js
+++ TeamXTicTacToe/ClientApp/src/components/Game.js
@@ -183,9 +183,23 @@ function calculateWinner(squares) {
     return null;
 }
 
+/** @function minimax
+ * @brief AI function that finds the best move according to the MiniMax algorithm.
+ * @param {string[]} reboard - The current TicTacToe board (this array is modified in the function).
+ * @param {string} player - The player whose turn it currently is (should be "X" or "O").
+ * @param {string} winningPlayer - The player that the algorithm is trying to make win.
+ * @returns {score | move} - If reboard is in a terminal state (win/lose/draw) when passed in,
+ *      returns an object with a property "score" that represents the MiniMax score. If reboard
+ *      has available spaces for moves, a move object representing the best move will be returned.
+ *      This object has two properties:
+ *          index - The index of the move
+ *          score - The associated MiniMax score
+ */
 function minimax(reboard, player, winningPlayer) {
-    // iter++; Don't need this?
-    let array = avail(reboard);
+    // Get a list of the available moves
+    let availableMoves = avail(reboard);
+
+    // Check for win/lose/draw
     var losingPlayer = winningPlayer === "X" ? "O" : "X";
     if (winning(reboard, losingPlayer)) {
         return {
@@ -195,17 +209,18 @@ function minimax(reboard, player, winningPlayer) {
         return {
             score: 10
         };
-    } else if (array.length === 0) {
+    } else if (availableMoves.length === 0) {
         return {
             score: 0
         };
     }
 
+    // Calculate minimax scores for every available move
     var moves = [];
-    for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < availableMoves.length; i++) {
         var move = {};
-        move.index = array[i];
-        reboard[array[i]] = player;
+        move.index = availableMoves[i];
+        reboard[availableMoves[i]] = player;
 
         if (player == winningPlayer) {
             var g = minimax(reboard, losingPlayer, winningPlayer);
@@ -214,10 +229,11 @@ function minimax(reboard, player, winningPlayer) {
             var g = minimax(reboard, winningPlayer, winningPlayer);
             move.score = g.score;
         }
-        reboard[array[i]] = null;
+        reboard[availableMoves[i]] = null;
         moves.push(move);
     }
 
+    // Find the move with the best score from the list of moves
     var bestMove;
     if (player === winningPlayer) {
         var bestScore = -10000;
