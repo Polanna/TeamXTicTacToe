75f6fd285d820fc885c1a2e098a7a0fd56f553b2
jonlee96 <jjlee015@gmail.com>
2020-11-19 15:51:03 +0900

    reverted all UI changes

--- TeamXTicTacToe/ClientApp/src/App.js
+++ TeamXTicTacToe/ClientApp/src/App.js
@@ -4,7 +4,7 @@ import { Layout } from './components/Layout';
 import Home from './components/Home';
 import { FetchData } from './components/FetchData';
 
-import NewGame from './components/NewGame';
+import { NewGame } from './components/NewGame';
 import { Settings } from './components/Settings';
 import { FAQPage } from './components/FAQPage';
 import { BoardPage } from './components/BoardPage';
@@ -14,14 +14,16 @@ export default class App extends Component {
 
     render() {
         return (
-            <Layout>
-                <Route exact path='/' component={NewGame} />
-                <Route path='/fetch-data' component={FetchData} />
-                <Route path='/new-game' component={NewGame} />
-                <Route path='/settings' component={Settings} />
-                <Route path='/faq' component={FAQPage} />
-                <Route path='/boardpage' component={BoardPage} />
-            </Layout>
+            <Fragment>
+                <Layout>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/fetch-data' component={FetchData} />
+                    <Route path='/new-game' component={NewGame} />
+                    <Route path='/settings' component={Settings} />
+                    <Route path='/faq' component={FAQPage} />
+                    <Route path='/boardpage' component={BoardPage} />
+                </Layout>
+            </Fragment>
         );
     }
 }
